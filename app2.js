0445cdeadd8c11c392a6e8f7479a36df3701577d
Mohnish nathani <nathanimohnish@gmail.com>
2020-12-30 13:11:59 +0530

    added batch to rest api

--- app2.js
+++ app2.js
@@ -3,52 +3,88 @@ const express = require('express');
 
 const app = express()
 
-const CONTRACT_ADDRESS = "0x7400313b94f5487fccb9f73bce25dc533bb47047";
+const CONTRACT_ADDRESS = "0x2c53e24933a56efd33338db401cabceaea10659a";
 const ABI = require('./abi.json'); 
+const provider = ethers.getDefaultProvider('goerli');
+const inter = new ethers.utils.Interface(ABI);
+const wallet = ethers.Wallet.fromMnemonic("regret zoo shed luggage tackle above reunion afraid dinosaur matrix orphan river").connect(provider);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
 
 
-
-app.get('/get', async(req, res)=>{
-    const provider = ethers.getDefaultProvider('goerli');
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+app.get('/getmed/:uuid', async(req, res)=>{
     try {
-        const value = await contract.returnMed();
+        const value = await contract.getMed(req.params.uuid);
+        console.log("Get data of uuid : "+req.params.uuid);
         res.send(value);
     } catch(e){
         res.send(e);
     }
 })
 
-app.get('/add', async (req, res)=>{
-    const provider = ethers.getDefaultProvider('goerli');
-    const wallet = ethers.Wallet.fromMnemonic("regret zoo shed luggage tackle above reunion afraid dinosaur matrix orphan river").connect(provider);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
+app.get('/getbatch/:batchid', async(req, res)=>{
     try {
-        await contract.addMed("12-12-2020","mkn","4","1","10","ok","man");
+        const value = await contract.getBatch(req.params.batchid);
+        console.log("Get batch of uuid : "+req.params.batchid);
+        const count = parseInt(value[1],16).toString();
+        const temp = parseInt(value[2],16).toString()
+        res.send({"uuid":value[0], "count":count, "temperature":temp});
+    } catch(e){
+        res.send(e);
+    }
+})
+
+app.get('/add/:batchid', async (req, res)=>{
+
+    try {
+        const value = await contract.getBatch(req.params.batchid);
+        const count = parseInt(value[1],16).toString();
+        const uuid = await contract.addMed(req.params.batchid,"Ovesh","1-10-20",5);
+        console.log("Added successfully");
         res.send("Successfully updated");
     } catch(e){
         res.send(e);
     }
 })
 
-app.get('/transactions',async(req,res)=>{
-    const provider = ethers.getDefaultProvider('goerli');
-    const wallet = ethers.Wallet.fromMnemonic("regret zoo shed luggage tackle above reunion afraid dinosaur matrix orphan river").connect(provider);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
-    const inter = new ethers.utils.Interface(ABI);
-    // provider.getTransaction('0xfc02c05c7cd09349afb913ef1059d0780211651c534ad914c5b6cb6253deabe6').then(function(tx) {
-    //          const de = inter.parseTransaction({data:tx.data, value:tx.value});
-    //          console.log(de); 
-    //         });
-    provider.getLogs({address:CONTRACT_ADDRESS}).then((mylog)=>{
-        // const decodedEvents = inter.decodeEventLog("Update",mylog[0].data);
-        // const decodedEvents = mylog.map(async (log) => await inter.decodeEventLog("Update",log.data)) ;
-        // console.log(decodedEvents);
-        mylog.forEach((log)=>{console.log(inter.decodeEventLog(log.topics[0],log.data))});
+app.get('/transactions/:uuid',async(req,res)=>{
+
+        var mylog = [];
+        var count = 1;
+        while(mylog.length==0){
+            console.log("Log attempt: "+count);
+            mylog = await provider.getLogs({address:CONTRACT_ADDRESS})
+            count++;
+        }
+        console.log("Logs Fetched Successfully");
+
+        var logarray = [];
+        mylog.forEach((log)=>{
+        var logdata = inter.decodeEventLog(log.topics[0],log.data)
+        if(logdata.uuid  === req.params.uuid){
+            logarray.push(logdata);
+        }
     })
-    
+    res.send(logarray);
 })
 
-
-
 app.listen(5000);
+
+// provider.getTransaction('0xfc02c05c7cd09349afb913ef1059d0780211651c534ad914c5b6cb6253deabe6').then(function(tx) {
+//          const de = inter.parseTransaction({data:tx.data, value:tx.value});
+//          console.log(de); 
+//         });
+
+// const decodedEvents = inter.decodeEventLog("Update",mylog[0].data);
+// const decodedEvents = mylog.map(async (log) => await inter.decodeEventLog("Update",log.data)) ;
+// console.log(decodedEvents);
+
+// provider.getLogs({address:CONTRACT_ADDRESS}).then((mylog)=>{
+//     var logarray = [];
+//     mylog.forEach((log)=>{
+//         var logdata = inter.decodeEventLog(log.topics[0],log.data)
+//         if(logdata.uuid  === req.params.uuid){
+//             logarray.push(logdata);
+//         }
+//     });
+//     res.send(logarray);
+// })
