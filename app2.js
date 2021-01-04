e1f42cb2f4e6b3e96b26801639492fbabc157c3a
Mohnish nathani <nathanimohnish@gmail.com>
2021-01-04 23:33:27 +0530

    added tracking in blockchain

--- app2.js
+++ app2.js
@@ -1,44 +1,69 @@
 const ethers = require('ethers');
 const express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express()
+app.use(bodyParser.json());
 
-const CONTRACT_ADDRESS = "0x2c53e24933a56efd33338db401cabceaea10659a";
+const CONTRACT_ADDRESS = "0x79f6e85bf724ec7c69c842fb6a212305ae3895e8";
 const ABI = require('./abi.json'); 
-const provider = ethers.getDefaultProvider('goerli');
+const provider = ethers.getDefaultProvider('rinkeby');
 const inter = new ethers.utils.Interface(ABI);
 const wallet = ethers.Wallet.fromMnemonic("regret zoo shed luggage tackle above reunion afraid dinosaur matrix orphan river").connect(provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
 
 
-app.get('/getmed/:uuid', async(req, res)=>{
+app.get('/getmed', async(req, res)=>{
     try {
-        const value = await contract.getMed(req.params.uuid);
-        console.log("Get data of uuid : "+req.params.uuid);
-        res.send(value);
+        const value = await contract.getMed(req.query.uuid);
+        console.log("Get data of uuid : "+req.query.uuid);
+        const temp = parseInt(value[4],16).toString();
+        res.send({"uuid":value[0], "batch_uuid":value[1],"name":value[2],"timestamp":value[3],"temperature":temp});
     } catch(e){
         res.send(e);
     }
 })
 
-app.get('/getbatch/:batchid', async(req, res)=>{
+app.get('/getbatch', async(req, res)=>{
     try {
-        const value = await contract.getBatch(req.params.batchid);
-        console.log("Get batch of uuid : "+req.params.batchid);
+        const value = await contract.getBatch(req.query.batchid);
+        console.log("Get batch of uuid : "+req.query.batchid);
         const count = parseInt(value[1],16).toString();
-        const temp = parseInt(value[2],16).toString()
+        const temp = parseInt(value[2],16).toString();
         res.send({"uuid":value[0], "count":count, "temperature":temp});
     } catch(e){
         res.send(e);
     }
 })
 
-app.get('/add/:batchid', async (req, res)=>{
+app.post('/track', async(req, res)=>{
+    try {
+        var batchid = req.body.batchid;
+        var temperature = req.body.temperature;
+        var pressure = req.body.pressure;
+        var lat = req.body.latitude;
+        var long = req.body.longitude;
+        var authority = req.body.authority;
+        var timestamp = req.body.timestamp;
+        var healthscore = req.body.healthscore;
+
+        await contract.addLog(batchid,temperature,pressure,lat,long,authority,timestamp,healthscore);
+        console.log("Tracking Post request submitted");
+        res.send("Tracking Post request submitted succesfully");
+    } catch(e){
+        res.send(e);
+    }
+})
+
+app.post('/add', async (req, res)=>{
 
     try {
-        const value = await contract.getBatch(req.params.batchid);
-        const count = parseInt(value[1],16).toString();
-        const uuid = await contract.addMed(req.params.batchid,"Ovesh","1-10-20",5);
+        var batchid = req.body.batchid;
+        var uuid = req.body.uuid;
+        var name = req.body.name;
+        var timestamp = req.body.mfd;
+        var temperature = req.body.temperature;
+        await contract.addMed(uuid,batchid,name,timestamp,temperature);
         console.log("Added successfully");
         res.send("Successfully updated");
     } catch(e){
@@ -46,7 +71,7 @@ app.get('/add/:batchid', async (req, res)=>{
     }
 })
 
-app.get('/transactions/:uuid',async(req,res)=>{
+app.get('/transactions',async(req,res)=>{
 
         var mylog = [];
         var count = 1;
@@ -60,12 +85,41 @@ app.get('/transactions/:uuid',async(req,res)=>{
         var logarray = [];
         mylog.forEach((log)=>{
         var logdata = inter.decodeEventLog(log.topics[0],log.data)
-        if(logdata.uuid  === req.params.uuid){
-            logarray.push(logdata);
+        if(logdata.uuid  === req.query.uuid){
+            // logarray.push(logdata);
+            logarray.push({"uuid":logdata[0], "name":logdata[1], "message":logdata[2]});
         }
     })
     res.send(logarray);
 })
+
+app.get('/transactions/batch',async(req,res)=>{
+
+    var mylog = [];
+    var count = 1;
+    while(mylog.length==0){
+        console.log("Log attempt: "+count);
+        mylog = await provider.getLogs({address:CONTRACT_ADDRESS})
+        count++;
+    }
+    console.log("Logs Fetched Successfully");
+
+    var logarray = [];
+    mylog.forEach((log)=>{
+    var logdata = inter.decodeEventLog(log.topics[0],log.data)
+    if(logdata.uuid  === req.query.batchid){
+        // logarray.push(logdata);
+        var temp = parseInt(logdata[1],16).toString();
+        var pressure = parseInt(logdata[2],16).toString();
+        var healthscore = parseInt(logdata[7],16).toString();
+        logarray.push({"batch_uuid":logdata[0], "temperature":temp, "pressure":pressure,"latitude":logdata[3],"longitude":logdata[4],"authority":logdata[5], "timestamp":logdata[6],"healthscore":healthscore});
+    }
+})
+res.send(logarray);
+console.log(logarray);
+})
+
+
 
 app.listen(5000);
 
